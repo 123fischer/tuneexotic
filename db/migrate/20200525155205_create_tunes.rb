37cf72da103067ebd48a917fcd176013eeef66e0
Montaser Amro <mono.amro@gmail.com>
2020-05-25 17:54:23 +0200

    creates tunes model

---
+++ db/migrate/20200525155205_create_tunes.rb
@@ -0,0 +1,12 @@
+class CreateTunes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tunes do |t|
+      t.string :name
+      t.string :description
+      t.string :url
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_151121) do
+ActiveRecord::Schema.define(version: 2020_05_25_155205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tunes", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "url"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tunes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +38,5 @@ ActiveRecord::Schema.define(version: 2020_05_25_151121) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tunes", "users"
 end
