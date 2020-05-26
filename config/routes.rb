d16308c32b3b9413f89c67b98c89fe2f35317bde
Esther Bernasconi <est.bernasconi+anmeldung@gmail.com>
2020-05-26 12:44:46 +0200

    adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tunes do
+    resources :requests, only: [:new, :create]
+    resources :favorites, only: [:new, :create, :show, :destroy]
+  end
+  patch "requests/:id/accept", to: "requests#accept", as: :accept_request
+  patch "requests/:id/reject", to: "requests#reject", as: :reject_request
 end
