016066da67bfb2053466af651bc0f16e56a95a1f
Montaser Amro <mono.amro@gmail.com>
2020-05-26 17:11:06 +0200

    Adds 20 faker files to our database through seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'pry-byebug'
+puts 'deleting all tunes'
+Tune.destroy_all
+
+puts 'creating tunes'
+20.times do
+  email = Faker::Internet.email
+  password = "123456"
+  name = "Test"
+  user = User.create(email: email, name: name, password: password)
+  name = Faker::Superhero.power
+  description = Faker::Lorem.sentences
+  url = Faker::Internet.domain_name
+  tune = Tune.new(name: name, description: description, url: url)
+  tune.user = user
+  tune.save
+  puts "#{name} is created"
+end
+
+puts 'Done creating tunes'
