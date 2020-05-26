29ed91c4bc113e37efc7ec9bfe34045bfb7c0153
Esther Bernasconi <est.bernasconi+anmeldung@gmail.com>
2020-05-26 17:42:51 +0200

    create request controller and updated request model & view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_092843) do
+ActiveRecord::Schema.define(version: 2020_05_26_152912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_05_26_092843) do
     t.bigint "tune_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status", default: "pending"
     t.index ["tune_id"], name: "index_requests_on_tune_id"
     t.index ["user_id"], name: "index_requests_on_user_id"
   end
