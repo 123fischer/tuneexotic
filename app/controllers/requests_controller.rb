bba799fea1095a279363134bd02c84229ad40d26
Montaser Amro <mono.amro@gmail.com>
2020-05-27 14:46:20 +0200

    Creates a new dashboard showing requests

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,30 +1,36 @@
+require 'pry-byebug'
 class RequestsController < ApplicationController
   before_action :set_tune, only: [:accepted, :rejected]
-    
+
   def create
     @request = Request.new
     @request.user = current_user
     @tune = Tune.find(params[:tune_id])
     @request.tune = @tune
+
     if @request.save
       redirect_to tune_path(@tune)
     else
       render :new
     end
   end
-    
-  def accepted
+
+  def accept
+    @request = Request.find(params[:id])
     @request.status = "accepted"
     @request.save
+    redirect_to dashboard_path
   end
 
-  def rejected
+  def reject
+    @request = Request.find(params[:id])
     @request.status = "rejected"
     @request.save
+    redirect_to dashboard_path
   end
 
   private
-    
+
   def set_request
     @request = Request.find(params[:id])
   end
